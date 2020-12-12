f928b4d4117c5f164e6d790280579f892324591b
bulhwi <mm225012@gmail.com>
2020-12-12 19:14:20 +0900

    현재 활성 scene 반영 로직 추가

--- js/main.js
+++ js/main.js
@@ -49,6 +49,17 @@
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[i].objs.container.style.height =  `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for(let i = 0; i<sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if(totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
   function scrollLoop() {
@@ -59,20 +70,25 @@
 
     if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
     if(yOffset < prevScrollHeight) {
+      if(currentScene === 0) {
+        return;
+      }
       currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
-    console.log(currentScene)
   }
-  //브라우저 크기가 변할때도 호출.
-  window.addEventListener("resize", setLayout);
+
   window.addEventListener("scroll", () => {
     // window.pageYOffset 현재 스크롤 값
     yOffset = window.pageYOffset;
     scrollLoop();
   })
 
-  setLayout();
+  //브라우저 크기가 변할때도 호출.
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })()
